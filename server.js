e84bef707607dbdfc3dd7ed8a8fb965cc3b83918
Anshuman Gupta <anshuman844@gmail.com>
2020-04-05 13:56:04 +0530

    Modified server.js file.   - Now connect to database.   - Routes `/login` and `/signup` added.   - Serverd index.html file at path `/`.

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 require('dotenv').config();
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-const router = express.Router();
+const login = require('./routes/login');
+const signup = require('./routes/signup');
 
-router.get('/', (req, res) => {
-  res.send(`<h1>Sarvekshan</h1>`);
+app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect(process.env.DATABASE, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}, (error) => { if (error) console.error("Connection Error : " + error); });
+
+const db = mongoose.connection;
+db.once('open', () => {
+  app.use('/login', login);
+  app.use('/signup', signup);
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 });
-
-app.use('/', router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
