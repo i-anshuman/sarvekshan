29290a34a2c6db823fec9563880850515eb502d5
Anshuman Gupta <anshuman844@gmail.com>
2020-04-05 22:19:02 +0530

    Module authentication created.   - It holds definition for serialize, deserialize and local-strategy.

---
+++ auth/index.js
@@ -0,0 +1,42 @@
+const bcrypt        = require('bcrypt');
+const passport      = require('passport');
+const mongoose      = require('mongoose');
+const LocalStrategy = require('passport-local');
+const ObjectID      = mongoose.Types.ObjectId;
+const Users         = require('../models/users');
+
+const authentication = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((userID, done) => {
+    Users.findOne({ _id: new ObjectID(userID) }, (error, user) => {
+      done(error, user);
+    });
+  });
+
+  passport.use(new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    (email, password, done) => {
+      Users.findOne({ email: email }, (error, user) => {
+        if (error) {
+          done(error);
+        }
+        if (!user) {
+          done(null, false, { message: "User not found." });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          done(null, false, { message: "Incorrect Password." });
+        }
+        else {
+          done(null, user);
+        }
+      }
+    );
+  }));
+}
+
+module.exports = authentication;
