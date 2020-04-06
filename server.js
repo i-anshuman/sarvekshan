f368aea3d1b892eac7ab9045ad43c800a96c497e
Anshuman Gupta <anshuman844@gmail.com>
2020-04-06 13:00:57 +0530

    Updated server.js file.   - To serve /login and /signup route.

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require('express');
-require('dotenv').config();
-const mongoose = require('mongoose');
+const express    = require('express');
+const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
-const app = express();
-const login = require('./routes/login');
-const signup = require('./routes/signup');
+const session    = require('express-session');
+const passport   = require('passport');
+const app        = express();
+const login      = require('./routes/login');
+const signup     = require('./routes/signup');
+const auth       = require('./auth');
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.connect(process.env.DATABASE, {
   useCreateIndex: true,
   useNewUrlParser: true,
@@ -15,9 +25,14 @@ mongoose.connect(process.env.DATABASE, {
 }, (error) => { if (error) console.error("Connection Error : " + error); });
 
 const db = mongoose.connection;
-db.once('open', () => {
+db.on('open', () => {
+  auth();
   app.use('/login', login);
   app.use('/signup', signup);
+
+  app.get('/profile', (req, res) => {
+    res.json({ data: req.user });
+  });
 
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
