31fe8ffef0967af7fa3a42c124f90db3cdd3da7f
Anshuman Gupta <anshuman844@gmail.com>
2020-04-07 16:16:43 +0530

    Middleware to verify inputs created.

---
+++ utils/middlewares.js
@@ -0,0 +1,30 @@
+const { isEmail, isName } = require('./validator');
+
+const verifyLoginInputs = (req, res, next) => {
+  const email = req.body.email;
+  if (isEmail(email)) {
+    next();
+  }
+  else {
+    res.json({ error: "Invalid email." });
+  }
+}
+
+const verifySignupInputs = (req, res, next) => {
+  const { fullname, email } = { ...req.body };
+  const errors = {};
+  if (!isName(fullname)) {
+    errors.name = "Invalid name.";
+  }
+  if (!isEmail(email)) {
+    errors.email = "Invalid email.";
+  }
+  if (Object.keys(errors).length > 0) {
+    res.json({ errors });
+  }
+  else {
+    next();
+  }
+}
+
+module.exports = { verifyLoginInputs, verifySignupInputs };
