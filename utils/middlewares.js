428c97ac43d3d153b853cf4d143a3cfcaf56c8c0
Anshuman Gupta <anshuman844@gmail.com>
2020-04-08 22:02:46 +0530

    Added middleware.   - To verify input submitted for creating new survey.

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,4 +1,4 @@
-const { isEmail, isName } = require('./validator');
+const { isName, isDate, isTime, isEmail, isTitle, isDescription } = require('./validator');
 
 const verifyLoginInputs = (req, res, next) => {
   const email = req.body.email;
@@ -27,6 +27,38 @@ const verifySignupInputs = (req, res, next) => {
   }
 }
 
+const verifySurveyInputs = (req, res, next) => {
+  const { title, description, validityDate, validityTime } = { ...req.body };
+  const errors = {};
+  if (!isTitle(title)) {
+    errors.title = "Invalid title format.";
+  }
+  else if (title.length < 5) {
+    errors.title = "Title too short."
+  }
+  if (!isDescription(description)) {
+    errors.description = "Invalid description format.";
+  }
+  else if (description.length < 100) {
+    errors.description = "Description must be at least 100 characters long.";
+  }
+  else if (description.length > 200) {
+    errors.description = "Description must be at most 200 characters long.";
+  }
+  if (!isDate(validityDate)) {
+    errors.date = "Invalid date format.";
+  }
+  if (!isTime(validityTime)) {
+    errors.time = "Invalid time format.";
+  }
+  if (Object.keys(errors).length > 0) {
+    res.json({ errors });
+  }
+  else {
+    next();
+  }
+}
+
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -36,4 +68,4 @@ const ensureAuthenticated = (req, res, next) => {
   }
 }
 
-module.exports = { verifyLoginInputs, verifySignupInputs, ensureAuthenticated };
+module.exports = { verifyLoginInputs, verifySignupInputs, verifySurveyInputs, ensureAuthenticated };
