079285fbd7370bdbddd6b66071bddc75e13da29f
Anshuman Gupta <anshuman844@gmail.com>
2020-04-13 14:20:26 +0530

    Methods added and exported.   - isQuetion(): to check if question doesn't have invalid character.   - isOption(): to check if option doesn't have invalid character.   - isValidOptionType(): to check if option type is valid.

--- utils/validator.js
+++ utils/validator.js
@@ -26,4 +26,16 @@ const isObjectID = (id) => {
   return /^[0-9a-f]{24}$/i.test(id);
 }
 
-module.exports = { isName, isEmail, isDate, isTime, isDescription, isTitle, isObjectID };
+const isQuestion = (question) => {
+  return /^[A-Za-z0-9 .:\(\)\[\]\{\}?',-]+$/.test(question);
+}
+
+const isOption = (option) => {
+  return isQuestion(option);
+}
+
+const isValidOptionType = (type) => {
+  return /^(checkbox|radio)$/.test(type);
+}
+
+module.exports = { isName, isEmail, isDate, isTime, isDescription, isTitle, isObjectID, isQuestion, isOption, isValidOptionType };
