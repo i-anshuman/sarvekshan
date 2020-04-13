ae3be19bcecf6fa2fd863e816e2436234ef3f7ed
Anshuman Gupta <anshuman844@gmail.com>
2020-04-13 14:25:04 +0530

    Added route to add new question.

--- routes/survey.js
+++ routes/survey.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const { add, list, view, addQuestion, publish, del, edit } = require('../controllers/survey');
-const { ensureAuthenticated, verifySurveyInputs, verifySurveyID } = require('../utils/middlewares');
+const { ensureAuthenticated, verifySurveyInputs, verifySurveyID, verifyQuestionInputs } = require('../utils/middlewares');
 
 router.post('/new', ensureAuthenticated, verifySurveyInputs, (req, res) => {
   const { validityDate, validityTime } = { ...req.body };
@@ -87,12 +87,14 @@ router.post('/edit/:surveyID', ensureAuthenticated, verifySurveyID, (req, res) =
   });
 });
 
-router.post('/edit/:surveyID/addQuestion', ensureAuthenticated, verifySurveyID, (req, res) => {
+router.post('/edit/:surveyID/addQuestion', ensureAuthenticated, verifySurveyID, verifyQuestionInputs, (req, res) => {
   const surveyID = req.params.surveyID;
   const userID = req.user._id;
-  const { question, type } = { ...req.body };
-  const newQuestion = { question, type };
-  addQuestion(surveyID, userID, newQuestion, (error, survey) => {
+  const { question, type, textbox } = { ...req.body };
+  const options = (req.body.options).map((option, index) => {
+    return (textbox && index === req.body.options.length - 1) ? { option: option, textbox: true } : { option: option };
+  });
+  addQuestion(surveyID, userID, { question, type, options }, (error, survey) => {
     if (error) {
       res.json({ error: error });
     }
