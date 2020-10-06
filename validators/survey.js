80a8df3651febc0089e3c77d4bd355a120e30ec5
Anshuman Gupta <anshuman844@gmail.com>
2020-10-06 21:29:29 +0530

    feat: Add validator to validate survey inputs

---
+++ validators/survey.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const { body, param } = require('express-validator');
+const { capitalize } = require('../utils');
+
+const valiateSurveyInputs = () => (
+  [
+    body('title', `Title must be at least 5 characters long and can only contains alphabets, digits, and some special symbols (. : , ' - ).`)
+      .trim().matches(/^[A-Za-z0-9 .:',-]+$/).isLength({min: 5}),
+    body('description', `Description must be 100-200 characters long and can only contains alphabets, digits, and some special symbols (. : , ' - ).`)
+      .trim().matches(/^[A-Za-z0-9 .:',-]+$/).isLength({min: 100, max: 200}),
+    body('validityDate', 'Must provide a valid date in YYYY-MM-DD format.')
+      .trim().isDate(),
+    body('validityTime', 'Must provide a valid time in HH:MM format')
+      .trim().matches(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/)
+  ]
+);
+
+const validateIDs = (...parameters) => {
+  return parameters.map(parameter => (
+    param(parameter, `${capitalize(parameter)} must be valid.`)
+      .isMongoId()
+  ));
+};
+
+const valiateQuestionInputs = () => (
+  [
+    body('question', `Question can only have alphabets, digits, brackets and some special symbols (. : , ' - ?).`)
+      .trim().matches(/^[A-Za-z0-9 .:\(\)\[\]\{\}?',-]+$/),
+    body('type', `Option type can be either 'checkbox' or 'radio'.`)
+      .trim().isIn(['checkbox', 'radio']),
+    body('options.*', `Options can only have alphabets, digits, brackets and some special symbols (. : , ' - ?).`)
+      .trim().matches(/^[A-Za-z0-9 .:\(\)\[\]\{\}?',-]+$/)
+  ]
+);
+
+const validatePublishStatus = () => (
+  [
+    param('state', 'Survey publish state must be boolean (true or false).')
+      .isBoolean()
+  ]
+);
+
+const validateSurveyEditInputs = () => (
+  [
+    body('title', `Title must be at least 5 characters long and can only contains alphabets, digits, and some special symbols (. : , ' - ).`)
+      .optional().trim().matches(/^[A-Za-z0-9 .:',-]+$/).isLength({min: 5}),
+    body('description', `Description must be 100-200 characters long and can only contains alphabets, digits, and some special symbols (. : , ' - ).`)
+      .optional().trim().matches(/^[A-Za-z0-9 .:',-]+$/).isLength({min: 100, max: 200}),
+    body('validityDate', 'Must provide a valid date in YYYY-MM-DD format.')
+      .optional().trim().isDate(),
+    body('validityTime', 'Must provide a valid time in HH:MM format')
+      .optional().trim().matches(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/)
+  ]
+);
+
+module.exports = {
+  validateIDs,
+  valiateSurveyInputs,
+  valiateQuestionInputs,
+  validatePublishStatus,
+  validateSurveyEditInputs
+};
