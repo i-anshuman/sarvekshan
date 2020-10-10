93e7b65f5740e12599afbf56c4b195f81e7024f4
Anshuman Gupta <anshuman844@gmail.com>
2020-10-10 22:07:08 +0530

    feat: Add methods to add, edit & del questions

---
+++ controllers/question.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Surveys  = require('../models/surveys');
+const mongoose = require('mongoose');
+const ObjectID = mongoose.Types.ObjectId;
+
+const add = (surveyID, userID, question, callback) => {
+  Surveys.findOneAndUpdate(
+    { _id: new ObjectID(surveyID), creatorID: new ObjectID(userID) }, 
+    { $push: { questions: question } }, 
+    { new: true }, 
+    (error, survey) => {
+      callback(error, survey);
+    }
+  );
+};
+
+const del = (surveyID, userID, questionID, callback) => {
+  Surveys.findOneAndUpdate(
+    { _id: new ObjectID(surveyID), creatorID: new ObjectID(userID) },
+    { $pull: { questions: { _id: questionID } } },
+    { new: true },
+    (error, survey) => {
+      callback(error, survey);
+    }
+  );
+};
+
+const edit = (surveyID, userID, questionID, rawData, callback) => {
+  const data = {};
+  for (const key in rawData) {
+    if (['question', 'type'].includes(key)) {
+      data[`questions.$.${key}`] = rawData[key];
+    }
+  }
+  Surveys.findOneAndUpdate(
+    {
+      _id: new ObjectID(surveyID),
+      creatorID: new ObjectID(userID),
+      questions: {
+        $elemMatch: {
+          _id: questionID
+        }
+      }
+    },
+    { $set: data },
+    { new: true },
+    (error, survey) => {
+      callback(error, survey);
+    }
+  );
+};
+
+module.exports = { add, del, edit };
